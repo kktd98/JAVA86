51a4636a3cbc2c658d7abad09187bbe2f5a030bb
kktd <1152075531@qq.com>
2020-04-10 17:47:09 +0800

    作业1

---
+++ day1/QQQ.java
@@ -0,0 +1,30 @@
+package day1;
+/**
+ * 
+ * @author nlc
+ * @date   2020年4月10日
+ */
+public class QQQ {
+     /*
+      * main是主程序入口  一个class文件只能出现一次
+      */
+        public static void main(String[] args){
+	
+    
+	String name="nlc";
+	int age =18;
+	double salary =5000.5;
+	String address ="苏州";
+	String hobbys ="球";
+	char sex = '男';
+	System.out.println("姓名: "+name);
+	System.out.println("年龄: "+age);
+	System.out.println("薪水: "+salary);
+	System.out.println("地址: "+address);
+	System.out.println("爱好: "+hobbys);
+	System.out.println("性别: "+sex);
+	
+	
+        }
+}
+
---
+++ day1/zuoYe1.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class zuoYe1 {
+	public static void main(String[] args){
+		String bus="公交车";
+		String hospital="医院";
+		String trainticket="火车票";
+		String planeticket="机票";
+		
+		
+		int bus_price=5;
+		int hospital_price=50;
+		int trainticket_price=500;
+		int planeticket_price=5000;
+		
+		System.out.println(bus+"车票价格为"+bus_price+"元.");
+		System.out.println(hospital+"挂号价格为"+hospital_price+"元.");
+		System.out.println(trainticket+"车票价格为"+trainticket_price+"元.");
+		System.out.println(planeticket+"车票价格为"+planeticket_price+"元.");
+		
+		}
+}
